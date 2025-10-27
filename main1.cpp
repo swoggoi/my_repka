b80eea2b732de770f5b37882099eb1f9774277ce
swoggoi <zxcasdqweff14@gmail.com>
2025-10-27 15:30:49 +0300

    main1

---
+++ main1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int t, x, count = 0;
+	cin >> x;
+	
+	t = x;
+	while (x != 0)
+	{
+		count++;
+		x /= 10;
+
+	}
+	--count;
+	while (t > 0)
+	{
+		x = x + (t % 10) * pow(10, count--);
+		t /= 10;
+	}
+	cout << x << endl;
+	system("pause");
+	return 0;
+}
